e4f9d781a62b41a478ca8a50596f34db6d154181
Jorja Hung <jorjahung@gmail.com>
2014-02-18 14:04:31 +0000

    Created Rock Paper Scissors app

--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -1,96 +1,73 @@
-describe("Rock-Paper-Scissors", function() {
-  var player1, player2, game;
-  
+describe("Player", function() {
+  var player;
   beforeEach(function() {
+    player = new Player('Player 1');
+  });
 
-    player1 = new Player('Player 1');
-    player2 = new Player('Player 2');
+  it('has a name', function() {
+    expect(player.name).toEqual('Player 1');
+  });
+
+  it('can pick', function() {
+    player.picks('Rock');
+    expect(player.pick.name).toEqual('Rock');
+  });
+});
+
+describe("Pick", function() {
+  it('has a name', function() {
+    var pick = new Pick('Rock');
+    expect(pick.name).toEqual('Rock');
+  });
+});
+
+describe("Game", function() {
+  it('has two players', function() {
+    var game = new Game('Joe', 'Martha');
+    expect(game.player1).toEqual('Joe');
+    expect(game.player2).toEqual('Martha');
+  });
+
+});
+
+describe('winning and losing', function() {
+  var player1, player2, game
+  beforeEach(function() {
+    player1 = new Player('Joe');
+    player2 = new Player('Bob');
     game = new Game(player1, player2);
+  });
+
+  it('rock should beat scissors', function() {
+    player1.picks('rock');
+    player2.picks('scissors');
+    expect(game.winner()).toBe('Rock smashes scissors. The winner is: JOE');
+  });
+
+  it('rock should lose to paper', function() {
+
+    player1.picks('rock');
+    player2.picks('paper');
+    expect(game.winner()).toBe('Paper covers rock. The winner is: BOB');
 
   });
 
-  describe('winning and losing', function() {
+  it('rock vs rock should be a draw', function() {
 
-    describe('rock', function() {
-
-      it('should beat scissors', function() {
-
-        player1.picks('rock');
-        player2.picks('scissors');
-        expect(game.winner()).toBe(player1);
-
-      });
-
-      it('should lose to paper', function() {
-
-        player1.picks('rock');
-        player2.picks('paper');
-        expect(game.winner()).toBe(player2);
-
-      });
-
-    });
-
-    describe('paper', function() {
-
-      it('should beat rock', function() {
-
-        player1.picks('paper');
-        player2.picks('rock');
-        expect(game.winner()).toBe(player1);
-
-      });
-
-      it('should lose to scissors', function() {
-
-        player1.picks('paper');
-        player2.picks('scissors');
-        expect(game.winner()).toBe(player2);
-
-      });
-
-    });
-
-    describe('scissors', function() {
-
-      it('should beat paper', function() {
-
-        player1.picks('scissors');
-        player2.picks('paper');
-        expect(game.winner()).toBe(player1);
-
-      });
-
-      it('should lose to rock', function() {
-
-        player1.picks('scissors');
-        player2.picks('rock');
-        expect(game.winner()).toBe(player2);
-
-      });
-
-    });
+    player1.picks('rock');
+    player2.picks('rock');
+    expect(game.winner()).toBe('Draw! You are both losers :P');
 
   });
 
-  describe('draws', function() {
 
-    describe('any identical picks', function() {
+  it('should have a choosing message', function() {
 
-      it('should result in no winner', function() {
-
-        var drawGameResults = ['rock', 'paper', 'scissors'].map(function(x) {
-          player1.picks(x);
-          player2.picks(x);
-          return game.winner();
-        });
-
-        expect(drawGameResults).toEqual([null, null, null]);
-
-      });
-
-    });
+    player1.picks('rock');
+    player2.picks('paper');
+    expect(game.choosing_message()).toBe('Joe chose rock. Bob chose paper.');
 
   });
 
+  
 });
---
+++ javascript/src/RockPaperScissors.js
@@ -0,0 +1,53 @@
+// Player Class
+function Player(name) {
+  console.log(name);
+  this.name = name;
+};
+
+Player.prototype.picks = function(playerPick) {
+  this.pick = new Pick(playerPick);
+}
+
+// Pick Class
+function Pick(name) {
+  this.name = name;
+};
+
+// Game Class
+function Game(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2
+};
+
+Game.prototype.rules = {
+  scissors: { paper: 'cuts', lizard: 'decapitates'},
+  paper:    { rock: 'covers', spock: 'disproves'},
+  lizard:   { spock: 'poisons', paper: 'eats' },
+  rock:     { scissors: 'smashes', lizard: 'crushes' },
+  spock:    { rock: 'vaporizes', scissors: 'melts' }
+};
+
+
+Game.prototype.choosing_message = function() {
+    return this.player1.name + " chose " + this.player1.pick.name + ". " + this.player2.name + " chose " + this.player2.pick.name + ".";
+};
+
+Game.prototype.winner = function() {
+  var player1_pick = this.player1.pick.name;
+  var player2_pick = this.player2.pick.name;
+  var player1_wins = this.rules[this.player1.pick.name][this.player2.pick.name];
+  var player2_wins = this.rules[this.player2.pick.name][this.player1.pick.name];
+ 
+
+  if(player1_wins) {
+    return player1_pick.charAt(0).toUpperCase() + player1_pick.slice(1) +" "+ player1_wins +" "+ player2_pick + ". The winner is: " + this.player1.name.toUpperCase();
+  }
+
+  if(player2_wins) {
+    return player2_pick.charAt(0).toUpperCase() + player2_pick.slice(1)  +" "+ player2_wins +" "+ player1_pick + ". The winner is: " + this.player2.name.toUpperCase();
+  } 
+
+  else {
+    return 'Draw! You are both losers :P';
+  };
+};
